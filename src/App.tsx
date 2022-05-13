f7a620a23e6388313d66d989517bc5940a84d4fc
Yu Hien <nyhdid@gmail.com>
2022-05-14 01:10:28 +0800

    Added simple esbuild transpiler service

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import * as esbuild from "esbuild-wasm";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  // to refer on specific var in fx component
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    // dig inside public directory, find esbuild compiled binaries
+    // access to webassembly service
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: "/esbuild.wasm",
     });
+
+    //#region output:
+    // {build: ƒ, serve: ƒ, transform: ƒ, stop: ƒ}
+    //   build: S => (g(), $.build(S))
+    //   serve: ƒ serve(S, k)
+    //   stop: ƒ stop()
+    //   transform: ƒ transform(S, k};
+    //#endregion
   };
 
-  const onClick = () => {
-    setCode(input);
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) return;
+
+    const res = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(res.code);
   };
 
   return (
