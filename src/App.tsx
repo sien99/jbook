9e0315e020502014e270ae08b1f07ff43464a24c
Yu Hien <nyhdid@gmail.com>
2022-05-18 01:29:31 +0800

    Added Code Editor MonacoEditor with styling and customization, migrate react monaco to latest version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import * as esbuild from "esbuild-wasm";
 import { useEffect, useRef, useState } from "react";
+import CodeEditor from "./components/code-editor";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 function App() {
   // to refer on specific var in fx component
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     // dig inside public directory, find esbuild compiled binaries
@@ -33,6 +34,9 @@ function App() {
   const onClick = async () => {
     if (!ref.current) return;
 
+    // To init iframe incase user deleted the root div for outputs
+    iframe.current.srcdoc = html;
+
     //build
     const result = await ref.current.build({
       entryPoints: ["index.js"],
@@ -47,29 +51,51 @@ function App() {
         global: "window",
       },
     });
-    console.log(result);
-    setCode(result.outputFiles[0].text);
-
-    // trycatch can't handle async function
-    (async (result) => {
-      try {
-        eval(result.outputFiles[0].text);
-      } catch (error) {
-        alert(error);
-      }
-    })(result);
+    // console.log(result);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message',e=> {
+          try {
+            
+            eval(e.data);
+          } catch (error) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red";><h4>Runtime Error</h4>'+ error + '</div>'
+            console.error(error);
+          }
+        },false)
+      </script>   
+    </body>
+  </html>
+  `;
 
   return (
     <div className='App'>
-      <h1>Mini Transpiler</h1>
+      <CodeEditor
+        initialValue='//Type Here!~'
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+
+      <iframe
+        title='preview'
+        ref={iframe}
+        srcDoc={html}
+        sandbox='allow-scripts'
+      />
     </div>
   );
 }
